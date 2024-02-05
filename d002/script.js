d9ca8e982c6b356be14939388ef6714dc4756225
Mono <rickyblesstf@gmail.com>
2024-02-05 10:54:47 -0300

    update commit
    Coloquei uma condição, para identificar se o valor da idade era igual a um número, e se não for, chamar a função novamente

--- d002/script.js
+++ d002/script.js
@@ -1,5 +1,10 @@
 function iniciarDesafio() {
     let nome = (window.prompt('Qual é o seu nome?'));
     let idade = Number(window.prompt(`Olá, ${nome}! Quantos anos você tem?`));
+    if (isNaN(idade)) {
+        window.alert('A quantidade que você tem de anos, é sempre um número. Por favor, preencha novamente!')
+        iniciarDesafio()
+    } else {
     window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`);
+    }
 }
